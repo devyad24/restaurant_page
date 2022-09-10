4644de3778db1d51defadece479f32d827c7820e
devyad24 <devyadav4211@gmail.com>
2022-09-10 14:27:06 +0530

    Add styling to pages.

--- src/index.js
+++ src/index.js
@@ -13,16 +13,29 @@ function tabSwitch() {
     const menuLink = document.querySelector('.menuLink');
     const contactLink = document.querySelector('.contactLink');
 
+    homeLink.classList.add('selectedLink');
+
     homeLink.addEventListener('click', () => {
         content.replaceChild(home(), content.lastChild);
+        if (!(homeLink.classList.contains('selectedLink'))) {
+            homeLink.classList.add('selectedLink');
+        }
+        menuLink.classList.remove('selectedLink');
+        contactLink.classList.remove('selectedLink');
     });
 
     menuLink.addEventListener('click', () => {
         content.replaceChild(menu(), content.lastChild);
+        homeLink.classList.remove('selectedLink');
+        menuLink.classList.add('selectedLink');
+        contactLink.classList.remove('selectedLink');
     });
 
     contactLink.addEventListener('click', () => {
         content.replaceChild(contact(), content.lastChild);
+        homeLink.classList.remove('selectedLink');
+        menuLink.classList.remove('selectedLink');
+        contactLink.classList.add('selectedLink');
     });
 }
 
